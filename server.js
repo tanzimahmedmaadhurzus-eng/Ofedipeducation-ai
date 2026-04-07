7e2f84e08e31d831851c0e9bc3a2abe7acc4cc79
tanzimahmedmaadhurzus-eng <tanzimahmedmaadhurz.us@gmail.com>
2026-04-07 11:37:23 +0600

    Create server.js

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("🧪 Chemistry AI Running 🚀");
+});
+
+app.post("/chemistry", (req, res) => {
+  const q = req.body.question.toLowerCase();
+  let answer = "";
+
+  if (q.includes("chloride")) {
+    answer = "Chloride (Cl⁻): AgNO3 → white ppt (AgCl)";
+  }
+  else if (q.includes("sulphate")) {
+    answer = "Sulphate (SO₄²⁻): BaCl2 → white ppt (BaSO4)";
+  }
+  else if (q.includes("carbonate")) {
+    answer = "Carbonate: acid → CO2 gas";
+  }
+  else if (q.includes("ammonium")) {
+    answer = "Ammonium: NaOH → NH3 gas";
+  }
+  else {
+    answer = "Ask chloride, sulphate, carbonate, ammonium";
+  }
+
+  res.json({ answer });
+});
+
+app.listen(3000, () => console.log("Running"));
